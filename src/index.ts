052e80684e28e08bdf0b8787b4ce19e685d1f235
djoyu <68707227+dj-oyu@users.noreply.github.com>
2025-11-11 20:53:05 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { isEven } from './isEven';
 
 export const checkEven = (num: number) => {
-    console.log('Checking if a number is even...');
+    console.log('Checking if a number is  even or odd.');
     const number = num;
     console.log(`Number ${number} is ${isEven(number) ? 'even' : 'odd'}`);
 };
